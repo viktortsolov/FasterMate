c46ff3337db03a084af28a31f0a0bf910262e7f2
Viktor Petrov Tsolov <viktortsolov03@gmail.com>
2022-03-05 13:09:50 +0200

    Added IsBeforeAttribute for DateTime

---
+++ FasterMate.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace FasterMate.Core.CustomAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string propertyToCompare;
+
+        public IsBeforeAttribute(string _propertyToCompare, string errorMessage = "")
+        {
+            propertyToCompare = _propertyToCompare;
+            this.ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+                    .ObjectType
+                    .GetProperty(propertyToCompare)
+                    .GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return new ValidationResult(ErrorMessage);
+
+        }
+    }
+}
